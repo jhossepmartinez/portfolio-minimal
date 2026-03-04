44c369c69d42b55ff4a7c04f6ee858299b154b28
jhossepmartinez <jhossepmv@gmail.com>
2026-03-03 21:13:18 -0300

    chore: make variables sensitive

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 variable "cloudflare_api_token" {
   description = "Cloudflare account api token"
+  sensitive   = true
   type        = string
 }
 
 variable "cloudflare_account_id" {
   description = "Cloudflare account id"
+  sensitive   = true
   type        = string
 }
 
