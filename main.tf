2043b9985e710ec6f1d84fc776c564bad51045a5
jhossepmartinez <jhossepmv@gmail.com>
2026-03-03 21:41:14 -0300

    chore: thingo

--- main.tf
+++ main.tf
@@ -8,4 +8,32 @@ resource "cloudflare_pages_project" "this" {
     build_command   = "npm run build"
     destination_dir = "out"
   }
+
+  # deployment_configs = {
+  #   production = {
+  #     environment_variables = {
+  #       NODE_VERSION = "22"
+  #     }
+  #   }
+  #   preview = {
+  #     environment_variables = {
+  #       NODE_VERSION = "22"
+  #     }
+  #   }
+  # }
+
+  source = {
+    type = "github"
+    config = {
+      owner                         = "jhossepmartinez"
+      repo_name                     = "portfolio-minimal"
+      production_branch             = "main"
+      pr_comments_enabled           = true
+      deployments_enabled           = true
+      production_deployment_enabled = true
+      preview_deployment_setting    = "custom"
+      preview_branch_includes       = ["*"]
+      preview_branch_excludes       = ["main"]
+    }
+  }
 }
